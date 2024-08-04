397415a08ec1943c3328a404896eb2653943337a
Colabres <pavel.allen@gmail.com>
2024-08-04 21:26:59 +0300

    2.4 rdy

--- OSA2/kurssitiedot/src/App.jsx
+++ OSA2/kurssitiedot/src/App.jsx
@@ -1,3 +1,4 @@
+import Course from './components/Course'
 const Part = ({ name, amount }) => {
   return (
     <p>
@@ -11,7 +12,7 @@ const Header = ({course}) => {
   return (
     <div>     
 
-      <h1>{course}</h1>
+      <h2>{course}</h2>
       
     </div>
   )
@@ -49,33 +50,69 @@ const Course =({course}) => {
   )
 }
 
+const Courses =({courses}) => {
+  return (
+    <div>
+      <h1>Web development curriculum</h1>      
+      {courses.map((course,i) => (     
+        <div key={i}>           
+        <Course course={course} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'State of a component 2',
-        exercises: 24
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   
   return (
     <div>
-    <Course course={course} />
+    <Courses courses={courses} />
   </div>
 
   )
