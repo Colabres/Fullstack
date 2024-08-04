aa712ffb5a127e6e76a7db9af6e2c16c7e4c053a
Colabres <pavel.allen@gmail.com>
2024-08-04 12:51:50 +0300

    1.6 rdy

--- OSA1/unicafe/src/App.jsx
+++ OSA1/unicafe/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-const Display = ({ counter }) => <div>{counter}</div>
+const Display = ({text ,counter }) => <div>{text} {counter}</div>
 
 const History = (props) => {
   if (props.allClicks.length === 0) {
@@ -25,29 +25,33 @@ const Button = ({ handleClick, text }) => (
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+
+  const handleGood = () => {    
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {    
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {    
+    setBad(bad + 1)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
 
   return (
     <div>
       <div>
-        {left}
-
-        <Button handleClick={handleLeftClick} text='left' />
-        <Button handleClick={handleRightClick} text='right' />
-        {right}
-        <History allClicks={allClicks} />
+        <h1>give feedback</h1>
+        <Button handleClick={handleGood} text='Good' />
+        <Button handleClick={handleNeutral} text='Neutral' />
+        <Button handleClick={handleBad} text='Bad' />        
+        <h1>statistics</h1>
+        <Display text="good" counter={good} />
+        <Display text="neutral" counter={neutral} />
+        <Display text="bad" counter={bad} />
       </div>
     </div>
   )
