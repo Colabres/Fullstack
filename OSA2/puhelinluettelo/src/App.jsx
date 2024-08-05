1d8cb7b55f034b0faae95f0618fb20d0e306cbd0
Colabres <pavel.allen@gmail.com>
2024-08-05 14:49:12 +0300

    2.9 rdy

--- OSA2/puhelinluettelo/src/App.jsx
+++ OSA2/puhelinluettelo/src/App.jsx
@@ -2,9 +2,7 @@ import Number from './components/Number'
 import React, { useState } from 'react';
 
 const App = (props) => {
-  // const [notes, setNotes] = useState(props.notes)
-  //const [newName, setNewNumber] = useState('a new entry...') 
-  // const [showAll, setShowAll] = useState(true)
+
 
   //works on submit
   const addNumber = (event) => {
@@ -37,20 +35,32 @@ const App = (props) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
-  // const notesToShow = showAll
-  //   ? notes
-  //   : notes.filter(note => note.important === true)
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewSearch(event.target.value)
+  }
+
+
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+
   const [newName, setNewName] = useState('*')
   const [newNumber, setNewNumber] = useState('*')
-
+  const [search, setNewSearch] = useState('')
+  const personsToShow = search === '' ? 
+  persons : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter show with: <input value={search} onChange={handleFilterChange}/></div>
+      <h2>add a new</h2>
       <form onSubmit={addNumber}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>        
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>        
@@ -59,7 +69,7 @@ const App = (props) => {
       <h2>Numbers</h2>
       ...
       <ul>
-        {persons.map(person =>
+        {personsToShow.map(person =>
           <Number key={person.name} name={person.name} number={person.number} />
         )}
       </ul>
