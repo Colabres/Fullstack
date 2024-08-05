1cf3e71748e8bc0b10708b33a89b6e51571961ee
Colabres <pavel.allen@gmail.com>
2024-08-05 22:38:19 +0300

    2.11 rdy

--- OSA2/puhelinluettelo/src/App.jsx
+++ OSA2/puhelinluettelo/src/App.jsx
@@ -2,8 +2,19 @@ import Filter from './components/Filter'
 import React, { useState } from 'react';
 import Numbers from './components/Number';
 import PersonForm from './components/PersonForm';
-const App = (props) => {
+import { useEffect } from 'react'
+import axios from 'axios'
 
+
+const App = (props) => {
+  
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   //works on submit
   const addNumber = (event) => {
@@ -44,12 +55,7 @@ const App = (props) => {
 
 
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('*')
   const [newNumber, setNewNumber] = useState('*')
   const [search, setNewSearch] = useState('*')
